b75bdb8f11d548e496d7e141fd4f8a942ec4dc8a
zhuaowei <z2845191711@163.com>
2021-11-23 22:06:24 +0800

    11.23第一次提交，添加单词统计和流量统计

---
+++ work01/src/main/java/com/word/count/first/WordCountReducer.java
@@ -0,0 +1,40 @@
+package com.word.count.first;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @ClassName: WordCountReducer
+ * @Description: 继承 Hadoop 的 Reducer，计算
+ * @Author: zhuaowei
+ * @Date: 2021/11/22
+ * @Version: 1.0
+ */
+
+public class WordCountReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    /**\
+     * 计算 map 中字段的个数
+     * @param key
+     * @param values
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        // 总和
+        int sum = 0;
+
+        // 遍历 key 的每个 value
+        for (IntWritable value : values) {
+            // 对 key 的所有 value 进行加和
+            sum += value.get();
+        }
+        // 将结果写入上下文，同样进行一下类型转换
+        context.write(key, new IntWritable(sum));
+    }
+}
---
+++ work02/src/main/java/com/word/count/first/WordCountReducer.java
@@ -0,0 +1,44 @@
+package com.word.count.first;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+/**
+ * @ClassName: WordCountReducer
+ * @Description: 继承 Hadoop 的 Reducer，计算
+ * @Author: zhuaowei
+ * @Date: 2021/11/22
+ * @Version: 1.0
+ */
+
+public class WordCountReducer extends Reducer<Text, Text, Text, Text> {
+
+    /**\
+     * 计算 map 中字段的个数
+     * @param key
+     * @param values
+     * @param context
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        // 总和
+        int uploadSum = 0;
+        int downloadSum = 0;
+
+        // 遍历 key 的每个 value
+        for (Text value : values) {
+            // 对 key 的所有 value 进行加和
+            System.out.println(value);
+            String[] upDown = value.toString().split("&");
+            uploadSum += Integer.parseInt(upDown[0]);
+            downloadSum += Integer.parseInt(upDown[1]);
+        }
+        int sum = uploadSum + downloadSum;
+        // 将结果写入上下文，同样进行一下类型转换
+        context.write(key, new Text(uploadSum + "\t" + downloadSum + "\t" + sum));
+    }
+}
